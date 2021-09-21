fce29163d87dcbc98c1ab4e3d3c73b841d03104a
Kirill Ivanov <55203533+kirill-ivanov-a@users.noreply.github.com>
2021-09-21 17:22:46 +0300

    Task 3. Graph to NFA #3
    -fuction graph_to_nfa and tests added.
    
    Co-authored-by: kirill-ivanov-a <ivanov.krirll.a@mail.com>

--- project/__init__.py
+++ project/__init__.py
@@ -4,5 +4,5 @@ from project.graph_utils import *
 import project.graph_info
 from project.graph_info import *
 
-import project.regex_utils
-from project.regex_utils import *
+import project.automaton_utils
+from project.automaton_utils import *
