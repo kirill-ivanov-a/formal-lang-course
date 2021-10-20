ef18f98dd338b3138637b8852d40233753361116
kirill-ivanov-a <55203533+kirill-ivanov-a@users.noreply.github.com>
2021-10-20 16:41:02 +0300

    epsilon checking
    checking that a CFG language contains epsilon added

--- project/cfg_utils.py
+++ project/cfg_utils.py
@@ -1,12 +1,22 @@
 """Utilities for working with CFG"""
-from pyformlang.cfg import CFG
+from pyformlang.cfg import CFG, Production
 
-__all__ = ["cfg_to_cnf", "cfg_from_file"]
+__all__ = ["cfg_to_wcnf", "cfg_from_file"]
 
 
-def cfg_to_cnf(cfg: CFG) -> CFG:
+def cfg_to_wcnf(cfg: CFG) -> CFG:
     """Converts a CFG to the Chomsky Normal Form"""
-    return cfg.to_normal_form()
+    wcnf = cfg.to_normal_form()
+    if cfg.generate_epsilon():
+        productions = set(wcnf.productions)
+        productions.add(Production(wcnf.start_symbol, []))
+        wcnf = CFG(
+            variables=wcnf.variables,
+            terminals=wcnf.terminals,
+            start_symbol=wcnf.start_symbol,
+            productions=productions,
+        )
+    return wcnf
 
 
 def cfg_from_file(path: str, start_symbol: str = "S") -> CFG:
--- tests/test_cfg_to_cnf.py
+++ tests/test_cfg_to_cnf.py
@@ -1,7 +1,11 @@
 import pytest
 from pyformlang.cfg import CFG
 
-from project import cfg_to_cnf
+from project import cfg_to_wcnf
+
+
+def is_wcnf(cfg: CFG) -> bool:
+    return all(p.is_normal_form() if p.body else True for p in cfg.productions)
 
 
 @pytest.fixture(
@@ -26,6 +30,14 @@ def cfg(request):
     return CFG.from_text(request.param)
 
 
-def test_cnf(cfg):
-    cnf = cfg_to_cnf(cfg)
-    assert cnf.is_normal_form()
+def test_wcnf(cfg):
+    wcnf = cfg_to_wcnf(cfg)
+    assert is_wcnf(wcnf)
+
+
+def test_eps_generation(cfg):
+    wcnf = cfg_to_wcnf(cfg)
+    if cfg.generate_epsilon():
+        assert wcnf.generate_epsilon()
+    else:
+        assert not wcnf.generate_epsilon()
