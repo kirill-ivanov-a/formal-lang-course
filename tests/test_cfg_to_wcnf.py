067c90628700d53a1a5bdb1da56a3fec925ce392
Kirill Ivanov <55203533+kirill-ivanov-a@users.noreply.github.com>
2021-10-23 17:45:50 +0300

    Task 6. CFG to CNF (#6)
    * Task 6 added
    
    - function for converting cfg to cnf;
    - function for reading cfg from a file.
    
    * codestyle fixed
    
    * epsilon checking
    
    checking that a CFG language contains epsilon added
    
    * func name changed
    
    * test name changed
    
    * docstring changed
    
    * conversion to wcnf changed
    
    * test for generated words

---
+++ tests/test_cfg_to_wcnf.py
@@ -0,0 +1,128 @@
+import pytest
+from pyformlang.cfg import CFG
+
+from project import cfg_to_wcnf
+
+
+def is_wcnf(cfg: CFG) -> bool:
+    return all(p.is_normal_form() if p.body else True for p in cfg.productions)
+
+
+def get_eps_generating_vars(cfg: CFG) -> set:
+    return {p.head.value for p in cfg.productions if not p.body}
+
+
+@pytest.fixture(
+    params=[
+        """
+    """,
+        """
+    N -> A B
+    """,
+        """
+    S -> epsilon
+    S -> a S b
+    S -> S S
+    """,
+        """
+    S -> epsilon
+    S -> a S b S
+    """,
+        """
+        A -> epsilon
+        B -> epsilon
+        C -> epsilon
+    """,
+    ]
+)
+def cfg(request):
+    return CFG.from_text(request.param)
+
+
+def test_wcnf(cfg):
+    wcnf = cfg_to_wcnf(cfg)
+    assert is_wcnf(wcnf)
+
+
+@pytest.mark.parametrize(
+    "cfg, exp_eps_gen_vars",
+    [
+        (
+            """
+            S -> S S | epsilon
+            """,
+            {"S"},
+        ),
+        (
+            """
+            S -> S S | A
+            A -> B | epsilon
+            B -> epsilon
+            """,
+            {"S"},
+        ),
+        (
+            """
+            S -> A a | S a
+            A -> epsilon
+            B -> epsilon
+            """,
+            {"A"},
+        ),
+    ],
+)
+def test_eps_generating(cfg, exp_eps_gen_vars):
+    wcnf = cfg_to_wcnf(CFG.from_text(cfg))
+    act_eps_gen_vars = get_eps_generating_vars(wcnf)
+    assert act_eps_gen_vars == exp_eps_gen_vars
+
+
+@pytest.mark.parametrize(
+    "cfg, contained_words",
+    [
+        (
+            """
+            S -> epsilon
+            A -> a | b | c | d
+            """,
+            {
+                True: [""],
+                False: ["a", "b", "c", "d"],
+            },
+        ),
+        (
+            """
+            S -> a S b S
+            S -> epsilon
+            """,
+            {
+                True: ["", "aaabbb", "abaabb", "ababab"],
+                False: ["abc", "aa", "bb", "ababa"],
+            },
+        ),
+        (
+            """
+            S -> i f ( C ) t h e n { ST } e l s e { ST }
+            C -> t r u e | f a l s e
+            ST -> p a s s | S
+            """,
+            {
+                True: [
+                    "if(true)then{pass}else{pass}",
+                    "if(false)then{if(true)then{pass}else{pass}}else{pass}",
+                    "if(false)then{pass}else{if(false)"
+                    "then{pass}else{if(true)then{pass}else{pass}}}",
+                ],
+                False: ["if(true)then{pass}else{pass", "", "if()then{}else{}"],
+            },
+        ),
+    ],
+)
+def test_generated_words(cfg, contained_words):
+    cfg = CFG.from_text(cfg)
+    wcnf = cfg_to_wcnf(cfg)
+    assert all(
+        wcnf.contains(w) and cfg.contains(w) for w in contained_words[True]
+    ) and all(
+        not wcnf.contains(w) and not cfg.contains(w) for w in contained_words[False]
+    )
