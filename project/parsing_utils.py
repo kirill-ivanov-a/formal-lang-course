53ce75dbb1ecdb1ed9a53bcb2b69f603e4ca59c6
Kirill Ivanov <55203533+kirill-ivanov-a@users.noreply.github.com>
2021-12-22 14:44:46 +0300

    Task 15. DOT printer (#14)
    * gql parser
    
    * Update test_gql_grammar.py
    
    * dot generator added
    
    * Update test_generate_dot.py
    
    * Update test_generate_dot.py
    
    * examples added
    
    * code style fixed
    
    * test data fixed
    
    * code style fixed

--- project/parsing_utils.py
+++ project/parsing_utils.py
@@ -1,9 +1,13 @@
-from antlr4 import InputStream, CommonTokenStream
+from antlr4.error.Errors import ParseCancellationException
+from antlr4.tree.Tree import TerminalNodeImpl
+from pydot import Dot, Node, Edge
 
+from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker, ParserRuleContext
 from project.gql_grammar.GraphQueryLanguageLexer import GraphQueryLanguageLexer
 from project.gql_grammar.GraphQueryLanguageParser import GraphQueryLanguageParser
+from project.gql_grammar.GraphQueryLanguageListener import GraphQueryLanguageListener
 
-__all__ = ["accept", "parse"]
+__all__ = ["accept", "parse", "generate_dot"]
 
 
 def parse(text: str) -> GraphQueryLanguageParser:
@@ -21,3 +25,37 @@ def accept(text: str) -> bool:
     parser.removeErrorListeners()
     _ = parser.prog()
     return parser.getNumberOfSyntaxErrors() == 0
+
+
+def generate_dot(text: str, path: str):
+    if not accept(text):
+        raise ParseCancellationException("The word doesn't match the grammar")
+    ast = parse(text).prog()
+    tree = Dot("tree", graph_type="digraph")
+    ParseTreeWalker().walk(
+        DotTreeListener(tree, GraphQueryLanguageParser.ruleNames), ast
+    )
+    tree.write(path)
+
+
+class DotTreeListener(GraphQueryLanguageListener):
+    def __init__(self, tree: Dot, rules):
+        self.tree = tree
+        self.num_nodes = 0
+        self.nodes = {}
+        self.rules = rules
+        super(DotTreeListener, self).__init__()
+
+    def enterEveryRule(self, ctx: ParserRuleContext):
+        if ctx not in self.nodes:
+            self.num_nodes += 1
+            self.nodes[ctx] = self.num_nodes
+        if ctx.parentCtx:
+            self.tree.add_edge(Edge(self.nodes[ctx.parentCtx], self.nodes[ctx]))
+        label = self.rules[ctx.getRuleIndex()]
+        self.tree.add_node(Node(self.nodes[ctx], label=label))
+
+    def visitTerminal(self, node: TerminalNodeImpl):
+        self.num_nodes += 1
+        self.tree.add_edge(Edge(self.nodes[node.parentCtx], self.num_nodes))
+        self.tree.add_node(Node(self.num_nodes, label=f"TERM: {node.getText()}"))
